55726f0c257631f234a9eb0a163590d3137f50b0
Sean Adrian Richard Vargas Cantor <71458406+SeanAdri64@users.noreply.github.com>
2026-01-14 22:55:42 -0500

    Update Comment.java

--- InyeccionDependenciasSpringBoot/src/main/java/com/example/claseInyec10Sping/model/Comment.java
+++ InyeccionDependenciasSpringBoot/src/main/java/com/example/claseInyec10Sping/model/Comment.java
@@ -28,6 +28,38 @@ public class Comment {
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
